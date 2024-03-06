f5cfad05424312aecf794dd3963da3475fe21d53
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 05:42:12 +0300

    Add files via upload

---
+++ 2.UNİTE UYGULAMA 26/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.UNİTE_UYGULAMA_26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for(int  i = 0; i < 5; i++)
+            {
+                double kuvvet=Math.Pow(10,i);
+                listBox1.Items.Add(kuvvet);
+            }
+        }
+    }
+}
